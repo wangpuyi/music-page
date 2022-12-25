f9f8ea96ef1d766946688dd02b3b82f6e7f95206
ztc136 <88202324+ztc136@users.noreply.github.com>
2022-12-25 16:19:46 +0800

    修改了引入方式，可以正常跳转出去了

--- src/views/C.vue
+++ src/views/C.vue
@@ -4,12 +4,7 @@
 </template> 
 <script>
 // 引入基本模板
-let echarts = require('echarts/lib/echarts')
-// 引入柱状图组件
-require('echarts/lib/chart/bar')
-// 引入提示框和title组件
-require('echarts/lib/component/tooltip')
-require('echarts/lib/component/title')
+let echarts = require('echarts')
 export default {
   name: 'hello',
   data() {
